1e347b971ee50947665ef415ce9e45242baa7868
Xsoda <Xsoda@live.com>
2015-10-30 15:18:05 +0800

    modify: pygments highlight style

--- conf.py
+++ conf.py
@@ -227,7 +227,7 @@ exclude_patterns = []
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'github'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
