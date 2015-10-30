311e085c4e4f7ead6739706d6ae5f20d52f4e9b1
Xsoda <Xsoda@live.com>
2015-10-30 10:08:03 +0800

    just continue testing autodeploy

--- conf.py
+++ conf.py
@@ -338,3 +338,7 @@ html_static_path = ['_static']
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'MyBlog-doc'
+
+# -- Options for travis deploy ----------------------------------------------
+
+github_pages = u'Xsoda'
