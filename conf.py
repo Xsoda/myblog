8d5846091bbb05f44f2aeb8d2603d2a50aac0dfb
Xsoda <Xsoda@live.com>
2015-12-25 17:04:28 +0800

    fix: theme option, image directory

--- conf.py
+++ conf.py
@@ -249,7 +249,11 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
+    'travis_button': True,
     'github_button': True,
+    'github_user': 'xsoda',
+    'github_repo': 'myblog',
+    'description': 'Thought & Learn & Remember'
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
