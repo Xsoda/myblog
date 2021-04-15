c6ada830d4e9a273d2d4b5a8c449880ae783ac09
Xsoda <Xsoda@live.com>
2021-04-15 16:16:30 +0800

    pygments style

--- conf.py
+++ conf.py
@@ -229,7 +229,7 @@ exclude_patterns = []
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'default'
+pygments_style = 'github'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
